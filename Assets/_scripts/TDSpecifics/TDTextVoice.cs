179608358bd34f9e78738d168fa6ccb2afe77088
Daniel Luque <danielluque14@gmail.com>
2021-03-04 03:29:31 +0100

    topdown: implementación inicial de diálogos

---
+++ Assets/_scripts/TDSpecifics/TDTextVoice.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using _scripts.UI.Language;
+using UnityEngine;
+
+public class TDTextVoice : MonoBehaviour
+{
+    [SerializeField] private AudioClip voiceText;
+    [SerializeField] private ComplexText text;
+
+    private void OnTriggerEnter2D(Collider2D collider)
+    {
+        Trigger();
+    }
+
+    public void Trigger()
+    {
+        TextWriter.Instance.WriteText(text.MainText.text, voiceText.length);
+        AudioSystem.SI.PlaySFX(voiceText);
+    }
+}
